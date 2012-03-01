ec395ce0bebe6317bc30848e0f45185311b50346
Kevin Gillette <extemporalgenome@gmail.com>
2012-02-29 21:48:30 -0700

    Initial commit.

---
+++ epochdate.go
@@ -0,0 +1,110 @@
+/*
+Represents dates from Jan 1 1970 - Jun 6 2149 as days since the Unix epoch.
+This format requires 2 bytes (it's a uint16), in contrast to the 16 or 20 byte
+representations (on 32 or 64-bit systems, respectively) used by the standard
+time package.
+
+Timezone is accounted for when applicable; when converting from standard time
+format into a Date, the date relative to the time value's zone is retained.
+Times at any point during a given day (relative to timezone) are normalized to
+the same date.
+
+Conversely, conversions back to standard time format may be done using the
+Local, UTC, and In methods (semantically corresponding to the same-named Time
+methods), but with the result normalized to midnight (the beginning of the day)
+relative to that timezone.
+*/
+package epochdate
+
+import (
+	"errors"
+	"time"
+)
+
+const (
+	day     = 60 * 60 * 24
+	maxUnix = (1<<16)*day - 1
+)
+
+const (
+	RFC3339       = "2006-01-02"
+	AmericanShort = "1-2-06"
+)
+
+var ErrOutOfRange = errors.New("The given date is out of range")
+
+type Date uint16
+
+func Today() Date {
+	date, err := NewFromTime(time.Now())
+	if err != nil {
+		panic(err)
+	}
+	return date
+}
+
+func Parse(layout, value string) (d Date, err error) {
+	t, err := time.Parse(layout, value)
+	if err == nil {
+		d, err = NewFromTime(t)
+	}
+	return
+}
+
+func NewFromTime(t time.Time) (Date, error) {
+	s := t.Unix()
+	_, offset := t.Zone()
+	return NewFromUnix(s + int64(offset))
+}
+
+func NewFromDate(year int, month time.Month, day int) (Date, error) {
+	return NewFromUnix(time.Date(year, month, day, 0, 0, 0, 0, time.UTC).Unix())
+}
+
+func NewFromUnix(seconds int64) (d Date, err error) {
+	if UnixInRange(seconds) {
+		d = Date(seconds / day)
+	} else {
+		err = ErrOutOfRange
+	}
+	return
+}
+
+func UnixInRange(seconds int64) bool {
+	return seconds >= 0 && seconds <= maxUnix
+}
+
+func (d Date) String() string {
+	return d.Format(RFC3339)
+}
+
+func (d Date) Format(layout string) string {
+	return d.UTC().Format(layout)
+}
+
+func (d Date) Date() (year int, month time.Month, day int) {
+	return d.UTC().Date()
+}
+
+func (d Date) UTC() time.Time {
+	return time.Unix(int64(d)*day, 0).UTC()
+}
+
+func (d Date) Local() time.Time {
+	return d.In(time.Local)
+}
+
+func (d Date) In(loc *time.Location) time.Time {
+	t := time.Unix(int64(d)*day, 0).In(loc)
+	_, offset := t.Zone()
+	return t.Add(time.Duration(-offset) * time.Second)
+}
+
+func (d Date) MarshalJSON() ([]byte, error) {
+	return []byte(d.Format(`"` + RFC3339 + `"`)), nil
+}
+
+func (d *Date) UnmarshalJSON(data []byte) (err error) {
+	*d, err = Parse(`"`+RFC3339+`"`, string(data))
+	return
+}
---
+++ epochdate_test.go
@@ -0,0 +1,100 @@
+package epochdate
+
+import (
+	"strings"
+	"testing"
+	"time"
+)
+
+type triple struct {
+	year  int
+	month time.Month
+	day   int
+}
+
+type equiv struct {
+	date Date
+	unix int64
+	trip triple
+	str  string
+}
+
+var equivs = []equiv{
+	{0, 0, triple{1970, 1, 1}, "1970-01-01"},
+	{0, day - 1, triple{1970, 1, 1}, "1970-01-01"},
+	{366, 366 * day, triple{1971, 1, 2}, "1971-01-02"},
+	{366, 367*day - 1, triple{1971, 1, 2}, "1971-01-02"},
+	{65535, 65535 * day, triple{2149, 6, 6}, "2149-06-06"},
+	{65535, 65536*day - 1, triple{2149, 6, 6}, "2149-06-06"},
+}
+
+var extrema = []struct {
+	unix  int64
+	valid bool
+}{
+	{-1, false},
+	{0, true},
+	{65536*day - 1, true},
+	{65536 * day, false},
+}
+
+func TestEquivalences(t *testing.T) {
+	for _, e := range equivs {
+		if unix, err := NewFromUnix(e.unix); err != nil {
+			t.Fatal(err)
+		} else if trip, err := NewFromDate(e.trip.year, e.trip.month, e.trip.day); err != nil {
+			t.Fatal(err)
+		} else if str, err := Parse(RFC3339, e.str); err != nil {
+			t.Fatal(err)
+		} else if e.date != unix || e.date != trip || e.date != str {
+			t.Fatal("Unexpected non-equivalence:", e.date, unix, trip, str)
+		}
+	}
+}
+
+func TestExtrema(t *testing.T) {
+	var desc string
+	for _, e := range extrema {
+		if UnixInRange(e.unix) != e.valid {
+			if e.valid {
+				desc = "valid"
+			} else {
+				desc = "invalid"
+			}
+			t.Fatal("Unix timestamp", e.unix, "should be", desc)
+		}
+	}
+}
+
+func TestTimezoneIrrelevance(t *testing.T) {
+	const hour = 60 * 60
+	min := time.FixedZone("min", -12*hour)
+	max := time.FixedZone("max", +14*hour)
+	t1 := time.Date(2149, 06, 06, 0, 0, 0, 0, min)
+	t2 := time.Date(2149, 06, 06, 0, 0, 0, 0, max)
+	var (
+		d1, d2 Date
+		err    error
+	)
+	if d1, err = NewFromTime(t1); err != nil {
+		t.Fatal(err)
+	}
+	if d2, err = NewFromTime(t2); err != nil {
+		t.Fatal(err)
+	}
+	if d1 != d2 {
+		t.Fatal("Expected", t1, "and", t2, "to result in same date; got", d1, "and", d2)
+	}
+}
+
+func TestDateToTime(t *testing.T) {
+	var date Date
+	local := date.Local()
+	utc := date.UTC()
+	prefix := "1970-01-01T00:00:00"
+	if f := local.Format(time.RFC3339); !strings.HasPrefix(f, prefix) {
+		t.Fatalf("Expected local time to %q; got %q\n", prefix, f)
+	} else if f := utc.Format(time.RFC3339); !strings.HasPrefix(f, prefix) {
+		t.Fatalf("Expected universal time to %q; got %q\n", prefix, f)
+	}
+}
